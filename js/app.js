3adc4b557ae6b152fb31531863c8145f55baa791
cris <a02003865@utmir.edu.mx>
2024-03-26 22:53:06 -0600

    Modificamos la ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 
+var url = window.location.href;
+var swLocation = '/chat_heroes/sw.js';
 
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js')
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js'
+    }
+    navigator.serviceWorker.register(swLocation)
 }
+
 // Referencias de jQuery
 
 var titulo      = $('#titulo');
